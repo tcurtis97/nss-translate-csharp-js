1aba5abfb19199b4682059fdf4ebe27c3a89a0ff
Tim Curtis <timcurtis97@yahoo.com>
2021-03-03 16:21:49 -0600

    finished place names

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,1 +1,23 @@
 // Put your code here
+
+console.log("Welcome to the main module");
+const names = [
+  "Nashville",
+  "Hong Kong",
+  "The back yard",
+  "Earth",
+  "London",
+  "The mall",
+  "Ryman Auditorium",
+  "The Great Wall of China",
+];
+
+console.log("All Place Names");
+
+const cities = names.forEach((i) => {
+  console.log(i);
+});
+
+let filteredNames = names.filter((f) => f.startsWith("The"));
+
+console.log(filteredNames);
