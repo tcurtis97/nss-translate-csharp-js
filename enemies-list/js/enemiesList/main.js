55a21c90095531cdabf6e9d6c81910fd92d802b7
Tim Curtis <timcurtis97@yahoo.com>
2021-03-03 16:25:27 -0600

    finished enemies list

--- enemies-list/js/enemiesList/main.js
+++ enemies-list/js/enemiesList/main.js
@@ -1,1 +1,52 @@
 // Put your code here
+
+const enemies = [
+  {
+    FirstName: "Joshua",
+    LastName: "Flowers",
+    Offenses: {
+      1: "Being a jerk to me in elementary school",
+      2: "Not being nice to me in elementary school",
+    },
+    IsReallyHated: true,
+  },
+  {
+    FirstName: "Darth",
+    LastName: "Vader",
+    Offenses: {
+      1: "Cut off Luke's hand",
+      2: "Murdered all those kids",
+      3: "Unkind management practices",
+    },
+    IsReallyHated: false,
+  },
+  {
+    FirstName: "Betty",
+    LastName: " Rudelady",
+    Offenses: {
+      1: "Phone calls in the theater",
+      2: "Phone calls on the bus",
+      3: "Phone calls in line at the grocery store",
+      4: "Poor conversationalist",
+    },
+    IsReallyHated: true,
+  },
+  {
+    FirstName: "Leon",
+    LastName: "Peck",
+    Offenses: {
+      1: "Keeps giving me a hotplate",
+    },
+    IsReallyHated: false,
+  },
+];
+
+console.log(enemies);
+
+for (const e of enemies) {
+  if (e.IsReallyHated === true) {
+    console.log(`${e.FirstName} ${e.LastName} Really, really dislike!`);
+  } else {
+    console.log(`${e.FirstName} ${e.LastName}`);
+  }
+}
