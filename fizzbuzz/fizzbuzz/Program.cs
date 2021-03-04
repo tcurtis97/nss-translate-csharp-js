908b920a168cd782fca435519f761a3841e86ff4
Tim Curtis <timcurtis97@yahoo.com>
2021-03-04 14:28:00 -0600

    finished fizzbuzz

---
+++ fizzbuzz/fizzbuzz/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void DoFizzBuzz()
+            {
+                for (int i = 1; i <= 100; i++)
+                {
+                    bool fizz = i % 3 == 0;
+                    bool buzz = i % 5 == 0;
+                    if (fizz && buzz)
+                        Console.WriteLine("FizzBuzz");
+                    else if (fizz)
+                        Console.WriteLine("Fizz");
+                    else if (buzz)
+                        Console.WriteLine("Buzz");
+                    else
+                        Console.WriteLine(i);
+                }
+            }
+
+            DoFizzBuzz();
+
+        }
+
+    }
+}
